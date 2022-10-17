bf38d79059345e6a899290d265da3c45535e38c3
FahadZaheerfzr <fahadzaheer19@gmail.com>
2022-10-17 22:03:00 +0500

    Documentation using Github Copilot

--- PlayfairCipher.py
+++ PlayfairCipher.py
@@ -1,23 +1,25 @@
-from base64 import encode
 from collections import OrderedDict
 
-
 class PlayfairCipher:
+    """Playfair Cipher Encryption"""
     def __init__(self, key, message):
         self.key = [i for i in key.upper()]
         self.matrix = []
         self.pairs = []
         self.encode(message)
 
+
     def create_matrix(self):
+        # Remove duplicates from key
         result = list(OrderedDict.fromkeys(self.key))
 
+        # Remove I and J from key
         for i in range(len(result)):
             if result[i] == "I" or result[i] == "J":
                 result[i] = "I/J"
 
         k = l = 0
-
+        
         alphabets = [
             "A",
             "B",
@@ -49,6 +51,7 @@ class PlayfairCipher:
         for char in result:
             alphabets.remove(char)
 
+        # Create 5x5 matrix
         for i in range(5):
             temp = []
             for j in range(5):
@@ -61,14 +64,17 @@ class PlayfairCipher:
             self.matrix.append(temp)
 
     def __print_matrix(self):
+        """Prints the matrix in a 5x5 format."""
         for i in range(5):
             for j in range(5):
                 print(self.matrix[i][j], end=" ")
             print()
 
     def format_message(self, message):
+        """Formats the message into pairs of characters."""
         message = message.upper().replace(" ", "")
         i = 0
+        # Add X if two consecutive characters are same and Add X at the end if last character is alone
         while i < len(message):
             if i + 1 < len(message):
                 if message[i] != message[i + 1]:
@@ -82,13 +88,17 @@ class PlayfairCipher:
                 i += 1
 
     def encode(self, message):
+        """Encodes the message using Playfair Cipher."""
         self.create_matrix()
         self.format_message(message)
         self.__print_matrix()
-        print()
-        print(self.pairs)
-        print()
+        print("\n"+ self.pairs + "\n")
         solutions = []
+        
+        # Find the index of the characters in the matrix
+        # If the characters are in the same row, replace them with the characters to their immediate right respectively
+        # If the characters are in the same column, replace them with the characters immediately below respectively
+        # If the characters are not on the same row or column, replace them with the characters on the same row respectively but at the other pair of corners of the rectangle defined by the original pair.
         for strings in self.pairs:
             index1 = self.__find_in_list_of_list(self.matrix, strings[0])
             index2 = self.__find_in_list_of_list(self.matrix, strings[1])
@@ -112,6 +122,7 @@ class PlayfairCipher:
         print(solutions)
 
     def __find_in_list_of_list(self, mylist, char):
+        """Returns the index of the first occurrence of char in mylist."""
         for sub_list in mylist:
             if char == "I" or char == "J":
                 if "I/J" in sub_list:
